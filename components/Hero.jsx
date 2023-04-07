c02d23bb4556293f7143ed9bf66777a889cc9f51
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-04-07 15:10:06 -0500

    hero component

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='h-screen flex items-center justify-center bg-slate-600'>
+        <p className='text-3xl text-blue-400'>
+            portfolio-v2
+        </p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <p className='text-3xl text-blue-400'>Portfolio - V2</p>
+    <div className='h-screen'>
+      <Head>
+        <title>Eddie Kuo</title>
+      </Head>
+
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
