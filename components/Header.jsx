2f6bb5944c248d63efb4a86bf5f39c687bd09497
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-04-07 15:22:16 -0500

    header layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        <p>
-            Header
-        </p>
-    </div>
+      <header className='flex justify-between px-20 py-5'>
+          <div className='flex'>
+              <p className='mr-5'>Home</p>
+              <p className='mx-5'>About</p>
+              <p className='ml-5'>Projects</p>
+          </div>
+          <div className='flex space-x-5'>
+              <p className='text-xl'>S</p>
+              <p className='text-xl'>S</p>
+              <p className='text-xl'>S</p>
+              <p className='text-xl'>S</p>
+              <p className='text-xl'>S</p>
+          </div>
+      </header>
   )
 }
