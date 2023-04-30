3641897b5b683daf8934c6aee35396f124b24f71
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-04-29 20:07:11 -0500

    minor ui tweaks

--- pages/about.js
+++ pages/about.js
@@ -10,17 +10,17 @@ export default function about() {
       <Head>
         <title>Eddie Kuo | About</title>
       </Head>
-      <main className='h-screen flex flex-col items-center pt-10 bg-light'>
+      <main className='h-screen flex flex-col items-center sm:pt-10 bg-light'>
         <div className='max-w-7xl p-5'>
           <motion.h3
-            initial={{ y: -30, opacity: 0 }}
+            initial={{ y: -50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className='uppercase tracking-[20px] text-xl font-bold text-center p-3'
+            className='uppercase tracking-[20px] text-xl font-bold text-center'
           >
             About Me
           </motion.h3>
-          <div className='flex flex-col md:flex-row items-center gap-5 w-full p-3 max-w-5xl mt-5'>
+          <div className='flex flex-col md:flex-row items-center gap-10 w-full p-3 max-w-6xl sm:mt-5'>
             <Image
               src='https://ozkuzfbdevqwhsqwknht.supabase.co/storage/v1/object/public/portfolio-pictures/FBD69CB0-F50D-4F79-AFCE-DEB49D9309ED_1_201_a.jpeg'
               alt='profile-picture'
@@ -28,24 +28,32 @@ export default function about() {
               height={500}
               className='w-60 h-60 rounded-full md:w-[350px] md:h-[450px] md:rounded-lg lg:w-[500px] lg:h-[600px]'
             />
-            <div className='text-base'>
+            <motion.div
+              initial={{ x: 200, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              className='text-base'
+            >
               Hi! My name is Eddie and I'm a full-stack software engineer based
               in Austin, Texas. With a specialty in Javascript and experience in
               building out full-stack applications from scratch, I have found a
               passion in writing software that is focused on delivering the best
-              user experience. What first intrigued me about tech was the power
-              we are given as users. All the information in the world rests
-              right in the palm of our hands. Now as the developer, I have made
-              it my goal to create applications that will in hopes, simplify
-              people's lives one line of code at a time.
-            </div>
-            {/* About me paragraph
-          Portrait/ photo  */}
+              user experience. <br /> What first intrigued me about tech was the
+              power we are given as users. All the information in the world
+              rests right in the palm of our hands. Now as the developer, I have
+              made it my goal to create applications that will in hopes,
+              simplify people's lives one line of code at a time.
+            </motion.div>
           </div>
-          {/* <h3 className='uppercase tracking-[20px] text 2xl font-bold'>
+          <motion.h3
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            className='uppercase tracking-[20px] text-xl font-bold text-center mt-10'
+          >
             Tech Stack
-          </h3>
-          <h3 className='uppercase tracking-[20px] text 2xl font-bold'>
+          </motion.h3>
+          {/* <h3 className='uppercase tracking-[20px] text 2xl font-bold'>
             Education
           </h3>
           <h3 className='uppercase tracking-[20px] text 2xl font-bold'>
