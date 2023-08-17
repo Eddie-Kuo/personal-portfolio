6531f8de72e46bf7a7a3c15d0285f077ee3b03f1
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-08-17 16:55:18 -0500

    fix additional projects spacing on mobile view

--- pages/projects.js
+++ pages/projects.js
@@ -50,7 +50,7 @@ export default function Projects() {
           >
             Other Notable Projects
           </motion.h3>
-          <div className='grid grid-cols-2 lg:grid-cols-3 gap-2 mb-32'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-32'>
             {notableProjects}
           </div>
         </div>
